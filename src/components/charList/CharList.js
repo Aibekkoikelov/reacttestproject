d990f03d5bcdd3229c22c2a5e2cb28810d10dfc0
Aibek <aibekkoikelov@gmail.com>
2021-11-30 23:30:54 +0600

    modify charlist.js file

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,52 +1,78 @@
-import './charList.scss';
-import abyss from '../../resources/img/abyss.jpg';
+import {Component} from 'react'
+import Spinner from '../spinner/Spinner'
+import ErrorMessage from '../errorMessage/ErrorMessage'
+import MarvelService from "../../services/MarvelService";
+import "./charList.scss";
 
-const CharList = () => {
-    return (
-        <div className="char__list">
-            <ul className="char__grid">
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item char__item_selected">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-            </ul>
-            <button className="button button__main button__long">
-                <div className="inner">load more</div>
-            </button>
-        </div>
-    )
+class CharList extends Component {
+    state = {
+        char: [],
+        loaded: true,
+        error: false
+    }
+    marvelservice = new MarvelService();
+
+    componentDidMount() {
+        this.marvelservice.getAllCharacters().then(this.onUpdateCharList).catch(this.onError);
+    }
+    onUpdateCharList = (char) => {
+        this.setState({
+            char,
+            loaded: false
+        })
+
+    }
+    onError = () => {
+        this.setState({
+            loading: false,
+            error: true
+        })
+    }
+
+    onRenderChar = (arr) => {
+        const items = arr.map(item => {
+            let imgStyle = { 'objectFit': 'cover' }
+            if (item.thumbnail.indexOf("image_not_available") > 0) {
+                imgStyle = { 'objectFit': 'unset' };
+            }
+            return (
+              <li className="char__item" key={item.id}>
+                <img src={item.thumbnail} alt={item.name} style={imgStyle} />
+                <div className="char__name">{item.name}</div>
+              </li>
+            );
+        })
+        return <ul className="char__grid">
+            {items}
+        </ul>;
+    }
+
+   
+    
+    render() {
+        const { char, loaded, error } = this.state
+        const items = this.onRenderChar(char)
+        const spinner = loaded ? <Spinner /> : null
+        const errorMessage = error ? <ErrorMessage /> : null
+        const content = !(loaded || error) ? items : null
+        
+
+        return (
+    <div className="char__list">
+                {errorMessage}
+                {spinner}
+                {content}
+      <button className="button button__main button__long">
+        <div className="inner">load more</div>
+      </button>
+    </div>
+  );
+
+    }
+
+  
 }
+
+
 
 export default CharList;
--- src/index.js
+++ src/index.js
@@ -2,15 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
 import './style/style.scss';
-import MarvelService from './services/MarvelService'
-
-const marvelService = new MarvelService();
-
- marvelService.getAllCharacters().then(result => result.data.results.forEach(character => console.log(character.name)))
-
-
-marvelService.getCharacter(1011334).then(result => console.log(result)
-);
 
 ReactDOM.render(
   <React.StrictMode>
